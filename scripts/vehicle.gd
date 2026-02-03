2d177f41221f8af4011dd6106518f0575a4569a0
flowerFawn <georgejparker08@Gmail.com>
2026-02-03 18:08:54 +0000

    basic mouse-seeking vehicles

---
+++ scripts/vehicle.gd
@@ -0,0 +1,37 @@
+extends Node2D
+class_name Vehicle
+
+##The maximum speed (per second) the vehicle can travel at
+@export var max_speed:float = 500
+##The max amount (per second) the vehicle can apply a force to it's velocity
+@export var force:float = 1000
+@export_category("technical")
+@export var node_sprite:Sprite2D
+
+##Where the vehicle currently wants to go
+var desired:Vector2
+##Current velocity the vehicle is moving at
+var velocity:Vector2 = Vector2.ZERO
+
+
+
+func _physics_process(delta: float) -> void:
+	do_process(delta)
+	
+func do_process(delta) -> void:
+	desired = get_desired()
+	velocity += get_acceleration() * delta
+	velocity.limit_length(max_speed)
+	node_sprite.rotation = velocity.angle()
+	move(delta)
+	
+func get_desired() -> Vector2:
+	return max_speed * get_local_mouse_position().normalized()
+	
+	
+func get_acceleration() -> Vector2:
+	return force * (desired - velocity).normalized()
+	
+
+func move(delta:float) -> void:
+	position += velocity * delta
